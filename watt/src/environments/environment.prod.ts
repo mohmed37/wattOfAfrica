7d714ed519138c18ec9d0fab038913bf7841d4b5
F20940 <mbehillil@yahoo.fr>
2020-09-15 13:26:37 +0200

    création d'une variable d'environement

--- watt/src/environments/environment.prod.ts
+++ watt/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   name: 'prod',
   version: 'v1.0',
-  backendServer:'http://localhost:8080/zuulservice'
+  backendServer:'http://www.wattsucces.com'
 };
