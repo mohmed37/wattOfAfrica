51fa9d4d2408d20b5e197802fa1507ea1c46ffac
F20940 <mbehillil@yahoo.fr>
2020-09-25 11:33:47 +0200

    mis à jour de l'Ip

--- watt/src/environments/environment.prod.ts
+++ watt/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   name: 'prod',
   version: 'v1.0',
-  backendServer:'https://51.158.67.65:8080/zuulservice'
+  backendServer:'http://51.158.67.65:8080/zuulservice'
 };
